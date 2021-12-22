0e4904f3ff92399d85d44462e42d0c2c589803ac
joe-nomad <1921731@iub.edu.bd>
2021-12-22 14:51:30 +0600

    log in log out

--- SEAS/settings.py
+++ SEAS/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'seasapp',
-    'blog',
+    'user',
     'crispy_forms',
     'django_nvd3',
    
@@ -129,7 +129,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'seas-home'
+LOGIN_URL = 'login'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- SEAS/urls.py
+++ SEAS/urls.py
@@ -15,18 +15,29 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from users import views as 
-
+from user import views as user_views
+from django.contrib.auth import views as auth_views
+from django.conf import settings 
+from django.conf.urls.static import static 
 from seasapp import populations,views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',populations.upload_file),
     path('success/',views.success),
-    path('classroom/',views.classroom),
+    path('classroom/',views.classroom,name='seas-home'),
     path('classroomTable/',views.classroom_requirement),
     path('resource_usage/',views.resource_usage),
     path('available_resource/',views.available_resource),
     path('availability_course_offering/',views.Availability_course_offering_comparison),
-    path('enrollment_wise_course_distribution/',views.Enrollment_wise_course_distribution)
+    path('enrollment_wise_course_distribution/',views.Enrollment_wise_course_distribution),
+    path('register/',user_views.register,name='register'),
+    # path('profile/',user_views.profile,name='profile'),
+    path('login/', auth_views. LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
+
+
+if settings.DEBUG:
+
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
