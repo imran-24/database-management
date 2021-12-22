1ee12f5ae105af0512ea1c5958ba7cdafc203108
joe-nomad <1921731@iub.edu.bd>
2021-12-22 15:14:17 +0600

    profile created

--- SEAS/urls.py
+++ SEAS/urls.py
@@ -32,7 +32,7 @@ urlpatterns = [
     path('availability_course_offering/',views.Availability_course_offering_comparison),
     path('enrollment_wise_course_distribution/',views.Enrollment_wise_course_distribution),
     path('register/',user_views.register,name='register'),
-    # path('profile/',user_views.profile,name='profile'),
+    path('profile/',user_views.profile,name='profile'),
     path('login/', auth_views. LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
@@ -40,4 +40,4 @@ urlpatterns = [
 
 if settings.DEBUG:
 
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
